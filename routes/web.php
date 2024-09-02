f64be5e1a12d1cc6f6929d824435840c24eb22b1
Ratih Paramita <significantratih@gmail.com>
2024-09-02 11:34:32 +0700

    Pertemuan 2-Praktikum 1

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,35 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//BASIC ROUTING
 Route::get('/', function () {
     return view('welcome');
+});
+Route::get('/hello', function(){
+    return 'Hello World';
+});
+Route::get('/world', function(){
+    return 'World';
+});
+Route::get('/', function () {
+    return ('Selamat Datang');
+});
+Route::get('/about', function () {
+    return ('2241760063<br>Ratih Paramita');
+});
+
+//ROUTE PARAMETERS
+Route::get('/user/{name}', function($name){
+    return 'Nama saya '.$name;
+});
+Route::get('/posts/{post}/comments/{comment}', function($postId, $commentId){
+    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+});
+Route::get('/articles/{id}', function($id){
+    return 'Halaman Artikel dengan ID '.$id;
+});
+
+//OPTIONAL PARAMETERS
+Route::get('/user/{name?}', function($name='John'){
+    return 'Nama saya '.$name;
 });
